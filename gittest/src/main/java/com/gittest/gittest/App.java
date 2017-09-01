ba4433959c15cc82f92dca9baf0c9c093617b004
Ajit <ajitnk2006@gmail.com>
2017-09-01 12:03:29 +0530

    Update App.java

--- gittest/src/main/java/com/gittest/gittest/App.java
+++ gittest/src/main/java/com/gittest/gittest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World github" );
+        System.out.println( "Hello World github changaed online or someone" );
     }
 }
